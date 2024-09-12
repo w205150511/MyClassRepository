d13846a2336c6a29d170e95d1a160fce82070de8
w205150511 <w205150511@student.hccs.edu>
2024-09-12 18:50:04 -0500

    Create CH3.cpp

---
+++ CH3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char ch1, ch2;
+  int X;
+  cout<<"Enter 2 characters and one int number:";
+  cin>>"You entered:"<<ch1<<", "<<ch2<<", and the number "<<x<<end;
+  return0;
+}
