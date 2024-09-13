eb66127fe9ce6a834322da39cd9ab5cc3374d779
w205150511 <w205150511@student.hccs.edu>
2024-09-12 19:09:18 -0500

    Update CH3.cpp

--- CH3.cpp
+++ CH3.cpp
@@ -5,6 +5,7 @@ int main()
   char ch1, ch2;
   int X;
   cout<<"Enter 2 characters and one int number:";
+  cin>>ch1>>ch2>>x;
   cin>>"You entered:"<<ch1<<", "<<ch2<<", and the number "<<x<<end;
   return0;
 }
